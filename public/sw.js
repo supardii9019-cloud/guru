18a5730e34d7621f360b4ee5ba51f93b953abed2
supardii9019-cloud <supardii9019@gmail.com>
2026-04-24 08:39:57 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'sekolah-digital-v1'
+const urlsToCache = [
+  '/guru/',
+  '/guru/index.html',
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  // Hanya cache GET requests
+  if (event.request.method !== 'GET') return
+  // Jangan cache Supabase API
+  if (event.request.url.includes('supabase.co')) return
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone))
+        return response
+      })
+      .catch(() => caches.match(event.request))
+  )
+})
